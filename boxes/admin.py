0eae6994c394cee04d25e58eaf71464f76bc40ab
jimbobding <jimmygrahamdavid@gmail.com>
2020-10-29 23:14:55 +0000

    customised admin

--- boxes/admin.py
+++ boxes/admin.py
@@ -3,5 +3,26 @@ from .models import Season, Box
 
 # Register your models here.
 
-admin.site.register(Season)
-admin.site.register(Box)
+
+class SeasonAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+    )
+
+
+class BoxAdmin(admin.ModelAdmin):
+    list_display = (
+        'season',
+        'box_name',
+        'price',
+        'description',
+        'size',
+        'fruit',
+        'veg',
+        'image',
+    )
+
+
+admin.site.register(Season, SeasonAdmin)
+admin.site.register(Box, BoxAdmin)
