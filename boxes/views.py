63afb1c6c64d283acdde537289a062b70b2eab44
jimbobding <jimmygrahamdavid@gmail.com>
2020-10-29 23:40:51 +0000

    added boxes views and template

--- boxes/views.py
+++ boxes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import Box
+
 # Create your views here.
+def all_boxes(request):
+    """ A view to show all products including sorting and searching quries  """
+
+    boxes = Box.objects.all()
+
+    context = {
+        'boxes': boxes
+    }
+
+    return render(request, "boxes/boxes.html", context)
