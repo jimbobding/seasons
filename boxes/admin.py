08a416cd2b8b0805cf76a67fa6af0e2e8c70cfc7
jimbobding <jimmygrahamdavid@gmail.com>
2020-10-31 14:48:43 +0000

    chanegd the season and box models and added the category model

--- boxes/admin.py
+++ boxes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Season, Boxes
+from .models import Season, Boxes, Category
 
 # Register your models here.
 
@@ -8,22 +8,36 @@ class SeasonAdmin(admin.ModelAdmin):
 
     list_display = (
         'name',
+        'fruit',
+        'veg',
+        'description',
+        'image',
+        'month_1',
+        'month_2',
+        'month_3',
     )
 
 
 class BoxesAdmin(admin.ModelAdmin):
     list_display = (
-        'season',
+        
         'box_name',
         'price',
         'description',
         'size',
         'fruit',
         'veg',
-        'image_url',
         'image',
+    )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
     )
 
 
 admin.site.register(Season, SeasonAdmin)
 admin.site.register(Boxes, BoxesAdmin)
+admin.site.register(Category, CategoryAdmin)
+
