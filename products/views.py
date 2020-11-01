3a76cf5f1a02c566dc6ed4ab34d91fb3fb0ab712
jimbobding <jimmygrahamdavid@gmail.com>
2020-11-01 23:51:28 +0000

    added cards to product html

--- products/views.py
+++ products/views.py
@@ -3,11 +3,14 @@ from .models import Product
 
 # Create your views here.
 
+
 def all_products(request):
     """ A view to see all products """
 
-    products = Product.object.all()
+    products = Product.objects.all()
 
     context = {
         'products': products
     }
+
+    return render(request, "products/products.html", context)
