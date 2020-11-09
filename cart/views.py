cc3f221a1470f51d6d7d668584f87bf959040379
jimbobding <jimmygrahamdavid@gmail.com>
2020-11-09 23:54:09 +0000

    added notification, css and othr messages for toasts. Also fixed  bug in context so the delivery percentage would divide and give correct delivery amount

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -14,16 +14,18 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """adds a quantity of product to the cart"""
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Added {product.name} to your cart')
+
     else:
         cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your bag')
+        messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -32,6 +34,7 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """adjusts the quantity of specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
 
     cart = request.session.get('cart', {})
@@ -46,11 +49,17 @@ def adjust_cart(request, item_id):
 
 def remove_from_cart(request, item_id):
     """Removes item form cart"""
+
+    product = get_object_or_404(Product, pk=item_id)
+
     try:
         cart = request.session.get('cart', {})
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} to your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
+        
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
