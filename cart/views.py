5e34e57ddf9e07d64bf0623b6dbb28440e9717b9
jimbobding <jimmygrahamdavid@gmail.com>
2020-11-09 21:36:53 +0000

    added adjust and remove from bag views and calc subtotal templte filter

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -9,16 +9,44 @@ def view_cart(request):
     return render(request, "cart/cart.html")
 
 
-def add_to_cart(request, product_id):
+def add_to_cart(request, item_id):
     """adds a quantity of product to the cart"""
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if product_id in list(cart.keys()):
-        cart[product_id] += quantity
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
     else:
-        cart[product_id] = quantity
-    
+        cart[item_id] = quantity
+
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """adjusts the quantity of specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Removes item form cart"""
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
