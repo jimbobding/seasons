28469592f285bc80122a9638f0ef967ccd2ebb9e
jimbobding <jimmygrahamdavid@gmail.com>
2020-11-13 12:14:08 +0000

    added profiles models, view , url and template

---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def profile(request):
+    """ Display the user's profile. """
+
+    template = 'profile/profile.html'
+    context = {}
+
+    return render(request, template, context)
