68945a1f642af18651db92a4c3da6fefb0febd8e
jimbobding <jimmygrahamdavid@gmail.com>
2020-11-24 22:39:23 +0000

    added bucket configs and static storage files

--- seasons/settings.py
+++ seasons/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -184,6 +185,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'seasons-farm-box-you'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 30
