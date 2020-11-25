9dd8229abf4c1d213c3e634a1db4281dc1be0bbc
jimbobding <jimmygrahamdavid@gmail.com>
2020-11-25 21:37:23 +0000

    corrected error in email backend

--- seasons/settings.py
+++ seasons/settings.py
@@ -223,10 +223,11 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'seasons@example.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smt.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULTL_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    
