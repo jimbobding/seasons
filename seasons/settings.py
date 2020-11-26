72907ac852f959d018f5608e67467f50e758a38d
jimbobding <jimmygrahamdavid@gmail.com>
2020-11-26 02:14:09 +0000

    fixed spelling mistake in default from email in settings.py

--- seasons/settings.py
+++ seasons/settings.py
@@ -229,7 +229,4 @@ else:
     EMAIL_HOST = 'smp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULTL_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-
-    
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
