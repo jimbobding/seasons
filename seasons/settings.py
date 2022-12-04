2dc69c23a766db95a7d5e80b47999be093d40438
jimbobding <jimmygrahamdavid@gmail.com>
2022-12-04 00:58:50 +0000

    changed indent, on render allowed host

--- seasons/settings.py
+++ seasons/settings.py
@@ -35,8 +35,8 @@ ALLOWED_HOSTS = ['seasons-farm-box-you.herokuapp.com', 'localhost', 'render.com'
 
  # Add Render.com URL to allowed hosts
  RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    if RENDER_EXTERNAL_HOSTNAME:
+        ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
